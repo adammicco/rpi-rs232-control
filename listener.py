16bc1cb423446ab82465e535588ae736501e85f5
Adam Micco <adam@adammicco.com>
2024-01-03 20:38:43 -0500

    first stab at listener

---
+++ listener.py
@@ -0,0 +1,15 @@
+import socketserver
+from http.server import BaseHTTPRequestHandler
+
+HOST = "localhost"
+PORT = "8080"
+
+class TCPHandler(socketserver.BaseRequestHandler):
+
+    def handle(self):
+        self.data = self.request.recv(1024).strip()
+        print(f"{self.client_address[0]}: {self.data}")
+
+if __name__ == "__main__":
+    with socketserver.TCPServer((HOST, PORT), TCPHandler) as server:
+        server.serve_forever()
