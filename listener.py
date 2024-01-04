d44d37d4b79a9018d8c3463bd39decee795398aa
Adam Micco <adam@adammicco.com>
2024-01-03 20:42:14 -0500

    add simple response

--- listener.py
+++ listener.py
@@ -8,6 +8,7 @@ class TCPHandler(socketserver.BaseRequestHandler):
 
     def handle(self):
         self.data = self.request.recv(1024).strip()
+        self.request.sendall(self.data.upper())
         print(f"{self.client_address[0]}: {self.data}")
 
 if __name__ == "__main__":
