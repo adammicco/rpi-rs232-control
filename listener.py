49c38abda98e43796bc287fba0373c17d5806d26
Adam Micco <adam@adammicco.com>
2024-01-05 00:12:45 -0500

    decode result

--- listener.py
+++ listener.py
@@ -9,7 +9,7 @@ class TCPHandler(socketserver.BaseRequestHandler):
     def handle(self):
         self.data = self.request.recv(1024).strip()
         self.request.sendall(self.data.upper())
-        print(f"{self.client_address[0]}: {self.data}")
+        print(f"{self.client_address[0]}: {self.data.decode('utf-8')}")
 
 if __name__ == "__main__":
     with socketserver.TCPServer((HOST, PORT), TCPHandler) as server:
