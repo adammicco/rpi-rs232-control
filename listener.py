4dd6f85627bfb8d04b637b7a43908ee74cbbcfd0
Adam Micco <adam@adammicco.com>
2024-01-05 00:56:49 -0500

    update listener to send rs232 commands

--- listener.py
+++ listener.py
@@ -1,15 +1,22 @@
+import glob
 import socketserver
 from http.server import BaseHTTPRequestHandler
+from rs232com import RS232C
 
 HOST = "raspberrypi.local"
 PORT = 8080
+
+BUS = glob.glob("/dev/ttyUSB*")[0]
+BAUD = 9600
+rs232 = RS232C(bus=BUS, baud=BAUD)
 
 class TCPHandler(socketserver.BaseRequestHandler):
 
     def handle(self):
         self.data = self.request.recv(1024).strip()
         self.request.sendall(self.data.upper())
-        print(f"{self.client_address[0]}: {self.data.decode()}")
+        print(f"{self.client_address[0]}: {self.data.decode('utf-8')}")
+        rs232.send_req(self.data.decode('utf-8'))
 
 if __name__ == "__main__":
     with socketserver.TCPServer((HOST, PORT), TCPHandler) as server:
--- rs232com.py
+++ rs232com.py
@@ -1,25 +1,25 @@
-from serial import Serial
+import serial
+import glob
 from time import sleep
 
-BUS = "COM0"
+BUS = glob.glob("/dev/ttyUSB*")[0]
 BAUD = 9600
 
-class RS232C(Serial):
+class RS232C(serial.Serial):
     def __init__(self, bus, baud, *args, **kwargs):
         self.bus = bus
         self.baud = baud
-        super().__init__(port=self.bus, baudrate=self.baud, *args, **kwargs)
+        super().__init__(port=self.bus, baudrate=self.baud, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE, bytesize=serial.EIGHTBITS, timeout=1, *args, **kwargs)
 
-    def listen(self):
-        data = self.read_until(b"\b\n")
-        return data
+    # TODO get reading from the RS-232C port working. Need to validate that anything is actually being sent
+    # def listen(self):
+    #     data = self.read_until(bytearray("\r\n", 'ascii'))
+    #     self.read()
+    #     return data
 
-    def send_req(self, payload, await_response=False):
-        self.write(bytearray(payload), 'ascii')
-        if await_response:
-            sleep(0.1)
-            self.listen()
+    def send_req(self, cmd, await_response=False, start_seq='!1', end_seq='\r\n'):
+        self.write(bytearray(f"{start_seq}{cmd}{end_seq}"), 'ascii')
+        # if await_response:
+        #     sleep(0.1)
+        #     self.listen()
         pass
-
-# if __name__ == "__main__":
-#     test = RS232C(BUS, BAUD)
