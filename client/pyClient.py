0a9a5b4b7494f0a0fbe009b1bac3aa99350e2664
adammicco <adam_micco@hms.harvard.edu>
2024-01-06 00:19:53 -0500

    functionize pyclient

--- client/pyClient.py
+++ client/pyClient.py
@@ -5,15 +5,19 @@ import argparse
 HOST = "raspberrypi.local"
 PORT = 8080
 
-parser = argparse.ArgumentParser()
-parser.add_argument('data', type=str)
-args = parser.parse_args()
-data = args.data.upper()
+def run(data):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+        sock.connect((HOST, PORT))
+        sock.sendall(bytes(data + "\n", 'utf-8'))
+        # rec = str(sock.recv(1024), 'utf-8')
+    pass
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-    sock.connect((HOST, PORT))
-    sock.sendall(bytes(data + "\n", 'utf-8'))
-    # rec = str(sock.recv(1024), 'utf-8')
+if __name__ =="__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('data', type=str)
+    args = parser.parse_args()
+    data = args.data.upper()
+    run(data)
 
 # print(f"Sent: {data}")
 # print(f"Recieved: {rec}")
